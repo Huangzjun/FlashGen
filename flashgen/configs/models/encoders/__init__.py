7f968f96e3d4df320c39e64e14a2e98c622c4def
huangyizhe <hyz22@noreply.gitcode.com>
2026-07-01 16:09:26 +0800

    Export image encoder config

--- flashgen/configs/models/encoders/__init__.py
+++ flashgen/configs/models/encoders/__init__.py
@@ -1,9 +1,15 @@
-from flashgen.configs.models.encoders.base import BaseEncoderOutput, EncoderConfig, TextEncoderConfig
+from flashgen.configs.models.encoders.base import (
+    BaseEncoderOutput,
+    EncoderConfig,
+    ImageEncoderConfig,
+    TextEncoderConfig,
+)
 from flashgen.configs.models.encoders.t5 import T5Config
 
 __all__ = [
     "BaseEncoderOutput",
     "EncoderConfig",
+    "ImageEncoderConfig",
     "TextEncoderConfig",
     "T5Config",
 ]
